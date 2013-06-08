9e8a71274d8bd6ba319892aecd018ca80e09d86d
Bartuz <iamnotawowplayer@gmail.com>
2013-06-08 14:45:08 -0400

    Fixed runtime feature to show weeks/hours/minutes/seconds/days runtime

--- features/clock.rb
+++ features/clock.rb
@@ -15,12 +15,17 @@ class Clock
 	end
 
 	def runtime
-		TimeDifference.between(start_time,Time.now).in_seconds
+		difference = TimeDifference.between(start_time,Time.new(2013,7,8,13,45))
+		runtime_in_secs=(difference.in_seconds%60).to_i
+		runtime_in_minutes=(difference.in_minutes%60).to_i
+		runtime_in_hours=(difference.in_hours%60).to_i
+		runtime_in_days=(difference.in_days%24).to_i
+		runtime_in_weeks=(difference.in_weeks%7).to_i
+		puts "Runs for #{runtime_in_weeks} weeks, #{runtime_in_days} days, #{runtime_in_hours} hours, #{runtime_in_minutes} minutes and #{runtime_in_secs} seconds"
 	end
 
 end
-#c=Clock.instance
-#puts c.start_time
-#puts c.display_start_time
-#sleep 20
-#puts c.runtime
+c=Clock.instance
+puts c.start_time
+puts c.display_start_time 
+puts c.runtime

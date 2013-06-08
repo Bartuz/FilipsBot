6861ae04800dfc9fa3397c76d24a7f13775b1755
Bartuz <iamnotawowplayer@gmail.com>
2013-06-08 14:52:22 -0400

    Clock.rb is fully functional now - shows runtime/startime and time now in friendly looking format

--- features/clock.rb
+++ features/clock.rb
@@ -1,4 +1,3 @@
-require 'singleton'
 require 'time_difference'
 class Clock
 	
@@ -11,11 +10,11 @@ class Clock
 	end
 
 	def display_start_time
-		"Started on #{start_time.inspect}"
+		"Started on #{time_print_format(start_time)}"
 	end
 
 	def runtime
-		difference = TimeDifference.between(start_time,Time.new(2013,7,8,13,45))
+		difference = TimeDifference.between(start_time,Time.new)
 		runtime_in_secs=(difference.in_seconds%60).to_i
 		runtime_in_minutes=(difference.in_minutes%60).to_i
 		runtime_in_hours=(difference.in_hours%60).to_i
@@ -24,8 +23,14 @@ class Clock
 		puts "Runs for #{runtime_in_weeks} weeks, #{runtime_in_days} days, #{runtime_in_hours} hours, #{runtime_in_minutes} minutes and #{runtime_in_secs} seconds"
 	end
 
+	def display_time_now
+		"Time is #{time_print_format(Time.now)}"
+	end
+
+	private
+
+	def time_print_format(time)
+		(time.inspect)[0..-6]
+	end
+
 end
-c=Clock.instance
-puts c.start_time
-puts c.display_start_time 
-puts c.runtime

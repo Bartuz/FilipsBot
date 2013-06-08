b85f866a5809745f0ccc13be3b620f895c16bef1
Bartuz <iamnotawowplayer@gmail.com>
2013-06-08 14:16:20 -0400

    Added working features to Clock.rb

--- features/clock.rb
+++ features/clock.rb
@@ -1,21 +1,26 @@
 require 'singleton'
-#require 'time_difference'
+require 'time_difference'
 class Clock
 	
 	include Singleton
 
-	attr_reader : :start_time
+	attr_reader :start_time
 
 	def initialize
-		@start_time = Time.now.new
+		@start_time = Time.now
 	end
 
 	def display_start_time
-		"BotStarted on #{start_time.inspect}"
+		"Started on #{start_time.inspect}"
 	end
 
 	def runtime
-	
+		TimeDifference.between(start_time,Time.now).in_seconds
 	end
 
 end
+#c=Clock.instance
+#puts c.start_time
+#puts c.display_start_time
+#sleep 20
+#puts c.runtime

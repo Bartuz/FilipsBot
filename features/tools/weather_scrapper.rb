18e4001574709e0b3d9564b98b4ff1c5da361127
Bartuz <iamnotawowplayer@gmail.com>
2013-06-09 00:00:55 -0400

    Weather and Weather scrapper is working after all tests. All displaying work well.

---
+++ features/tools/weather_scrapper.rb
@@ -0,0 +1,62 @@
+require 'nokogiri'
+require 'open-uri'
+class Weather_Scrapper
+
+	attr_accessor :city, :page, :url
+
+	def initialize (city)
+		@city = city
+		@url = "http://www.ask.com/web?q=#{city}+weather&search=&qsrc=0&o=0&l=dir"
+						#THERE IS --->   ^^^^^^^  <---- city variable.
+		@page = Nokogiri::HTML(open(url)).at_css(".sa")
+	end
+
+
+	def update(new_city_name)
+		unless @city == new_city_name
+			update_city(new_city_name)
+			update_url(new_city_name)
+		end
+		update_page
+	end
+
+	def current_weather
+		"Temperature in #{city} is #{current_temp[0]} (that is #{current_temp[1]}).\nConditions are #{current_conditions}."
+	end
+
+	def future_days(scope)
+		days = []
+		scope.times do |day|
+			day_night_f=future_weather_arrays[day-1][1].split("/")
+			day_night_C=future_weather_arrays[day-1][2].split("/")
+			days << "#{future_weather_arrays[day][0]} will be #{day_night_f[0]}F day and #{day_night_f[1]}F night (Thats is #{day_night_C[0]}C day and #{day_night_C[1]}C night.)"
+		end	
+		days
+	end
+
+	private
+
+	def update_city(city)
+		@city=city
+	end
+
+	def future_weather_arrays
+		page.at_css(".sa_forecasts").text.split.each_slice(3).to_a
+	end
+
+	def update_url(name_of_city)
+		@url = "http://www.ask.com/web?q=#{name_of_city}+weather&search=&qsrc=0&o=0&l=dir"
+	end
+
+	def update_page
+		@page = Nokogiri::HTML(open(url)).at_css(".sa")
+	end
+
+	def current_temp
+		page.at_css(".sa_weather_temp").text.split
+	end
+
+	def current_conditions
+		page.at_css(".sa_weather_condition").text.strip.downcase
+	end
+end
--- features/weather.rb
+++ features/weather.rb
@@ -1,11 +1,22 @@
-require 'singleton'
-require 'tools/weather.rb'
+require_relative 'tools/weather_scrapper'
 class Weather
 
-	include Singleton
+	attr_accessor :data
 
-	def initalize
-		@data = weather_scrapper.instance
+	def initialize(name_of_city = "Torotno")
+		@data = Weather_Scrapper.new(name_of_city)
 	end
+
+	def display_today
+		@data.current_weather
+	end
+
+	def display_future(long = 3)
+		(@data.future_days(long)).each { |line|  puts line}
+	end
+
+	def reload(city = @data.city)
+		@data.update(city)
+	end 
 
 end
